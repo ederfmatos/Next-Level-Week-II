2311d4d92d7a4d4741143eddbe639bade6002b7d
ederfmatos <ederfmatos@gmail.com>
2020-08-03 12:06:21 -0300

    FEAT: Add routes and pages TeacherList, TeacherForm

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 import './assets/styles/global.css';
 
-import { Landing } from './pages';
+import Routes from './routes';
 
 function App() {
-  return <Landing />;
+  return <Routes />;
 }
 
 export default App;
---
+++ web/src/pages/TeacherList/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function TeacherList() {
+  return <h1>Teacher List</h1>;
+}
