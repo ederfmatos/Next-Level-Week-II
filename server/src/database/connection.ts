e067c391a989cd8c8318de1f9b21f94c2f4cd870
ederfmatos <ederfmatos@gmail.com>
2020-08-04 19:15:50 -0300

    FEAT: Create migrations

---
+++ server/src/database/connection.ts
@@ -0,0 +1,10 @@
+import knex from 'knex';
+import { resolve } from 'path';
+
+const db = knex({
+  client: 'sqlite3',
+  connection: {
+    filename: resolve(__dirname, 'database.sqlite'),
+  },
+  useNullAsDefault: true,
+});
