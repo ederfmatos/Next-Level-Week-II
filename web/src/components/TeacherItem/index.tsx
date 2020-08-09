18ffeb152b0fc8fd1202791d96e075466457faa4
ederfmatos <ederfmatos@gmail.com>
2020-08-09 20:57:14 -0300

    FEAT: Add list of teachers

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,33 +3,37 @@ import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
 
-const TeacherItem: React.FC = () => {
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
   return (
     <article className="teacher-item">
       <header>
-        <img
-          src="https://avatars3.githubusercontent.com/u/34350634?s=460&u=0944410b12a53de8c311ee36e0e50fc147c1b83d&v=4"
-          alt="Eder Matos"
-        />
+        <img src={teacher.avatar} alt={teacher.name} />
 
         <div>
-          <strong>Eder Matos</strong>
-          <span>Matemática</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
-      <p>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged.
-      </p>
+      <p>{teacher.bio}</p>
 
       <footer>
         <p>
-          Preço/hora <strong>R$ 95.00</strong>
+          Preço/hora <strong>R$ {teacher.cost.toFixed(2)}</strong>
         </p>
 
         <button type="button">
--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,23 +1,84 @@
-import React from 'react';
+import React, { useCallback, FormEvent, useState } from 'react';
+import { useFormik } from 'formik';
+import { toast } from 'react-toastify';
+
 import { PageHeader, Input, Select } from '../../components';
 
-import './styles.css';
-import TeacherItem from '../../components/TeacherItem';
+import TeacherItem, { Teacher } from '../../components/TeacherItem';
 import { Subjects, WeekDays } from '../../constants';
+import api from '../../services.api';
+import './styles.css';
+
+interface FilterProps {
+  week_day: number;
+  subject: string;
+  time: string;
+}
 
 export default function TeacherList() {
+  const [teacherItems, setTeacherItems] = useState([]);
+  const formik = useFormik<FilterProps>({
+    initialValues: {
+      week_day: 0,
+      subject: '',
+      time: '',
+    },
+    onSubmit: () => {},
+  });
+
+  const searchTeachers = useCallback(
+    async (event: FormEvent) => {
+      event.preventDefault();
+
+      try {
+        const { data } = await api.get('/classes', {
+          params: formik.values,
+        });
+
+        setTeacherItems(data);
+      } catch (error) {
+        toast.error('Ocorreu um erro ao buscar professores');
+      }
+    },
+    [formik.values],
+  );
+
   return (
     <div id="page-teacher-list" className="container">
       <PageHeader title="Esses são os proffys disponíveis.">
-        <form id="search-teachers">
-          <Select name="subject" label="Matéria" options={Subjects} />
-          <Select name="week_day" label="Dia da semana" options={WeekDays} />
-          <Input name="time" label="Hora" type="time" />
+        <form id="search-teachers" onSubmit={searchTeachers}>
+          <Select
+            name="subject"
+            label="Matéria"
+            options={Subjects}
+            onChange={formik.handleChange}
+            value={formik.values.subject}
+          />
+
+          <Select
+            name="week_day"
+            label="Dia da semana"
+            options={WeekDays}
+            onChange={formik.handleChange}
+            value={formik.values.week_day}
+          />
+
+          <Input
+            name="time"
+            label="Hora"
+            type="time"
+            onChange={formik.handleChange}
+            value={formik.values.time}
+          />
+
+          <button type="submit">Buscar</button>
         </form>
       </PageHeader>
 
       <main>
-        <TeacherItem />
+        {teacherItems.map((teacher: Teacher) => (
+          <TeacherItem key={teacher.id} teacher={teacher} />
+        ))}
       </main>
     </div>
   );
