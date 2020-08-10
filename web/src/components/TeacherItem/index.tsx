bd3ea3b4030d0df897079e31c1c82944d09bfe0c
ederfmatos <ederfmatos@gmail.com>
2020-08-09 21:01:45 -0300

    FEAT: Add link to whatsapp to teacher

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services.api';
 
 export interface Teacher {
   id: number;
@@ -18,6 +19,10 @@ interface TeacherItemProps {
 }
 
 const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  const createNewConnection = useCallback(() => {
+    api.post('connections', { user_id: teacher.id });
+  }, [teacher.id]);
+
   return (
     <article className="teacher-item">
       <header>
@@ -36,10 +41,15 @@ const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
           Preço/hora <strong>R$ {teacher.cost.toFixed(2)}</strong>
         </p>
 
-        <button type="button">
+        <a
+          target="_blank"
+          onClick={createNewConnection}
+          rel="noopener noreferrer"
+          href={`https://wa.me/${teacher.whatsapp}`}
+        >
           <img src={whatsappIcon} alt="Icone do whatsapp" />
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
